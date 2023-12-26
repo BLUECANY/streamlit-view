176a16501e3f8035c5be6551350c0bacfb34778d
BLUECANY <66871913+BLUECANY@users.noreply.github.com>
2023-12-26 21:44:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ floor = 1
 button1 = st.button('1관')
 button2 = st.button('2관')
 button3 = st.button('3관')
+
 if button1:
   built = 1
 if button2:
   built = 1
 if button3:
   built = 1
+st.write(button)
