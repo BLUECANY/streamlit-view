23168104f1e60785ca0d929aa78d085c01e94363
BLUECANY <66871913+BLUECANY@users.noreply.github.com>
2023-12-26 21:43:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+built = 1
+floor = 1
+
 button1 = st.button('1관')
 button2 = st.button('2관')
 button3 = st.button('3관')
+if button1:
+  built = 1
+if button2:
+  built = 1
+if button3:
+  built = 1
