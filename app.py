470cdd1665115262d19443b74dbc904466859da6
BLUECANY <66871913+BLUECANY@users.noreply.github.com>
2023-12-26 21:38:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
-view = [100, 150, 30]
-st.write('# Youtube view')
-st.write('## raw')
-view
-st.write('## bar chart')
-st.bar_chart(view)
-import pandas as pandas
+
+button1 = st.button('1관')
+button2 = st.button('2관')
+button3 = st.button('3관')
